1d632ac3227e70e625362bf7d1a4dc2adcf49ec2
tylersisia <tyler@pdssystems.com>
2019-11-25 17:00:06 -0500

    Update CnUtils.ts

--- lib/CnUtils.ts
+++ lib/CnUtils.ts
@@ -2,7 +2,7 @@
 //
 // Please see the included LICENSE file for more information.
 
-import { CryptoNote } from 'turtlecoin-utils';
+import { CryptoNote } from 'chimera-utils';
 import { Config } from './Config';
 
 /**
